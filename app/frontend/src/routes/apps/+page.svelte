3206c80bc23516aece000c5bd2c5ffbe72cc0196
EnigmaCurry <ryan@enigmacurry.com>
2025-04-26 11:34:28 -0600

    apps page invalidation

--- app/frontend/src/routes/apps/+page.svelte
+++ app/frontend/src/routes/apps/+page.svelte
@@ -1,13 +1,24 @@
 <script>
+  import { invalidateAll } from '$app/navigation';
   import AppsTable from "$lib/AppsTable.svelte";
   import { currentContext } from "$lib/stores";
-  export let data;
+  let { data } = $props();
+  const ConfigKey = $derived(currentContext);
+
+  $effect(() => {
+    if ($currentContext != null) {
+      console.log("invalidate page");
+      invalidateAll(); // 🔥 re-run load(), refresh props!
+    }
+  })
+
 </script>
 
 <svelte:head>
   <title>dry_agent - Available Apps</title>
 </svelte:head>
 
+{#key ConfigKey}
 {#if $currentContext != "default" && $currentContext != null}
   {#if data.configExists}
     <h1 class="title">Available Apps</h1>
@@ -22,4 +33,4 @@
   <h1 class="title">No Context</h1>
   <a href="/docker" class="button is-link">Create and/or set a default Docker context</a>
 {/if}
-
+{/key}
